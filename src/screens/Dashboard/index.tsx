4e90211f66248256408ee5624d3a4321dea75e55
douglasdl <douglas_san@hotmail.com>
2021-09-06 12:23:19 -0300

    Add transactions flat list

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 import { HighlightCard } from '../../components/HighlightCard';
 import { TransactionCard } from '../../components/TransactionCard';
 
@@ -15,9 +16,39 @@ import {
     HighlightCards,
     Transactions,
     Title,
+    TransactionsList,
 } from './styles';
 
 export function Dashboard() {
+    const data = [
+        {
+            title: "Desenvolvimento de Site",
+            amount: "R$12.000,00",
+            category: {
+                name: "Vendas",
+                icon: "dollar-sign"
+            },
+            date: "13/04/2020"
+        },
+        {
+            title: "Desenvolvimento de Site",
+            amount: "R$12.000,00",
+            category: {
+                name: "Vendas",
+                icon: "dollar-sign"
+            },
+            date: "13/04/2020"
+        },
+        {
+            title: "Desenvolvimento de Site",
+            amount: "R$12.000,00",
+            category: {
+                name: "Vendas",
+                icon: "dollar-sign"
+            },
+            date: "13/04/2020"
+        },
+    ];
     return (
         <Container>
             <Header>
@@ -57,7 +88,15 @@ export function Dashboard() {
             <Transactions>
                 <Title>Listagem</Title>
 
-                <TransactionCard />
+                <TransactionsList
+                    data={data}
+                    renderItem={({ item }) => <TransactionCard data={item} />}
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{
+                        paddingBottom: getBottomSpace() + 20
+                    }}
+                />
+   
             </Transactions>
         </Container>
     );
