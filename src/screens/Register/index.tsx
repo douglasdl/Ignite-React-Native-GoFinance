7b5d833472d67b9bd10c642ec6d832b3f1d2921a
douglasdl <douglas_san@hotmail.com>
2021-09-09 10:57:22 -0300

    Add modal menu to the select button

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
+import { Modal } from 'react-native';
 import { Button } from "../../components/Form/Button";
-import { CategorySelect } from "../../components/Form/CategorySelect";
 import { Input } from "../../components/Form/Input";
 import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
+import { CategorySelectButton } from "../../components/Form/CategorySelectButton";
+import { CategorySelect } from "../../screens/CategorySelect";
+
 import { 
     Container,
     Header,
@@ -14,10 +17,24 @@ import {
 
 export function Register() {
     const [transactionType, setTransactionType] = useState('');
+    const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+    const [category, setCategory] = useState({
+        key: 'category',
+        name: 'Categoria',
+    });
 
     function handleTransactionTypeSelect(type: 'up' | 'down') {
         setTransactionType(type);
     }
+
+    function handleOpenSelectCategoryModal() {
+        setCategoryModalOpen(true);
+    }
+
+    function handleCloseSelectCategoryModal() {
+        setCategoryModalOpen(false);
+    }
+
     return (
         <Container>
             <Header>
@@ -51,12 +68,23 @@ export function Register() {
                         
                     </TransactionTypes>
 
-                    <CategorySelect title="Categoria" />
+                    <CategorySelectButton 
+                        title={category.name} 
+                        onPress={handleOpenSelectCategoryModal}
+                    />
 
                 </Fields>
 
                 <Button title="Enviar" />
             </Form>
+
+            <Modal visible={categoryModalOpen}>
+                <CategorySelect 
+                    category={category}
+                    setCategory={setCategory}
+                    closeSelectCategory={ handleCloseSelectCategoryModal }
+                />
+            </Modal>
         </Container>
     );
 }
